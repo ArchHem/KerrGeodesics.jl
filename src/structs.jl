301b3740856c08b9ba657c0a0406457cb9c05b59
ArchHem <84734676+ArchHem@users.noreply.github.com>
2025-10-19 23:29:49 +0100

    removing redundancies

--- src/structs.jl
+++ src/structs.jl
@@ -9,10 +9,29 @@ struct BatchInfo{V}
 
 end
 
+#This compues specialized, heuretical timesteps in the Kerr spacetime.
+#It uses the following heuretic:
+#If r2 > threshold, dt = mind(flat + r2 * outer_scaling, max)
+#If r2 < threshold, dt = min(max, flat + inner_scaling/r2)
+#To presevre continuity at the threshold, we enforce:
+# threshold * outer_scaling = inner_scaling / threshold
+
 struct TimeStepScaler{T}
-    min::T
+    max::T
     flat::T
     outer_scaling::T
     inner_scaling::T
     threshold::T
+    stop::T
+    maxtimesteps::Int32
+end
+
+function TimeStepScaler(max::T, flat::T, outer_scaling::T, threshold::T, stop::T, maxtimesteps::Int32) where T
+    inner_scaling = threshold * threshold * outer_scaling
+    return TimeStepScaler{T}(max, flat, outer_scaling, inner_scaling, threshold, stop, maxtimesteps)
+end
+
+function get_dt(r2::T, s::TimeStepScaler{T}) where T
+    dt = ifelse(r2 > s.threshold, min(s.max, s.outer_scaling * r2), min(s.max, s.inner_scaling / r2))
+    return dt
 end
